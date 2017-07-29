91aaf940952154031e78289e242d1ace2e4e8cb6
Dima Brodsky <Dima Brodsky>
2017-07-29 07:29:58 +0400

    automatic push

--- actions/input.city.rent/context_input.city.rent.js
+++ actions/input.city.rent/context_input.city.rent.js
@@ -2,7 +2,6 @@
 
 const data = require('../../data.js');
 const jsonQuery = require('json-query')
-const apiai = require("apiai");
 
 
 module.exports = {
@@ -32,40 +31,4 @@ module.exports = {
     parameterscontextout["distination_list_of_neighborhoods"]=neighborhoods.join(',');
     return parameterscontextout; 
   } 
-}
-
-// set variable values for user session in api ai
-var SetNeighborhoodEntityValues=function(sessionId,neighborhoods){
-
-    var app = apiai("b266cf849ba2485a96dcdcee069f60d2");
-
-    var sessionId = sessionId;
-
-    var user_entities = [{
-        name: 'Neighborhood',
-        extend: false,
-        entries: [
-        ]
-    }];
-
-    neighborhoods.forEach(function(neighborhood){
-        user_entities[0].entries.push({value:neighborhood.name,synonyms:[neighborhood.name]});
-    })
-    var user_entities_body = {
-        sessionId: sessionId,
-        entities: user_entities
-    };
-
-    var user_entities_request = app.userEntitiesRequest(user_entities_body);
-
-    user_entities_request.on('response', function(response) {
-        console.log('User entities response: ');
-        console.log(JSON.stringify(response, null, 4));
-    });
-
-    user_entities_request.on('error', function(error) {
-        console.log(error);
-    });
-
-    user_entities_request.end();
 }
