2e345014f44b94160dbe7c3e18f723ba025afaff
Dima Brodsky <Dima Brodsky>
2017-08-02 14:25:17 +0100

    automatic push

--- actions/input.food.eatingout/context_input.food.eatingout.js
+++ actions/input.food.eatingout/context_input.food.eatingout.js
@@ -18,10 +18,12 @@ module.exports = {
     parameterscontextout["distination_city_cost_of_eating_once_out_a_week_out_per_month"]=food_cost.cost_of_eating_once_out_a_week_out_per_month;
     parameterscontextout["distination_city_cost_of_not_bringing_food_from_home"]=food_cost.cost_of_not_bringing_food_from_home;
     parameterscontextout["distination_city_cost_of_grocery"]=food_cost.monthly_average_cost;
+    parameterscontextout["total_food_cost"]=parameterscontextout["distination_city_cost_of_grocery"]+parameterscontextout["distination_city_cost_of_eating_once_out_a_week_out_per_month"]*parameterscontextout["number_of_goingout_per_week"];
     parameterscontextout["city_from_cost_of_eating_once_out_a_week_out_per_month"]=city_from_food_cost.cost_of_eating_once_out_a_week_out_per_month;
     parameterscontextout["city_from_cost_of_not_bringing_food_from_home"]=city_from_food_cost.cost_of_not_bringing_food_from_home;
     parameterscontextout["city_from_cost_of_grocery"]=city_from_food_cost.monthly_average_cost;
     parameterscontextout["cooking_at_home"]=7-parameterscontextout["number_of_goingout_per_week"];
+    parameterscontextout["total_cost"]=parameterscontextout["distination_cost_of_rent_general"] + parameterscontextout["cooking_at_home"] + parameterscontextout["transport_cost"];
 
 
     return parameterscontextout; 
