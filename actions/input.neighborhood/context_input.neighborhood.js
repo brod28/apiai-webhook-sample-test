f70de447ea0368c3692021e011933cdf603889f7
Dima Brodsky <Dima Brodsky>
2017-08-02 14:11:36 +0100

    automatic push

--- actions/input.neighborhood/context_input.neighborhood.js
+++ actions/input.neighborhood/context_input.neighborhood.js
@@ -13,8 +13,8 @@ module.exports = {
                         element.cities.forEach(function(city) {
                             if(parameterscontextout["distination_city"]==city.name){
                                 city.neighborhoods.forEach(function(neighborhood) {
-                                    parameterscontextout["neighborhood_why_to_love_it"]=neighborhood.why_to_love_it;
                                     if(neighborhood.name==requestBody.result.parameters["distination_neighborhood"]){
+                                        parameterscontextout["neighborhood_why_to_love_it"]=neighborhood.why_to_love_it;
                                         parameterscontextout["distination_neighborhood_commute_time"]=neighborhood.commute_time;
                                         neighborhood.cost_of_rent.forEach(function(cost_of_rent) {
                                             parameterscontextout["cost_of_rent_per_neighborhood"]+=" in "
