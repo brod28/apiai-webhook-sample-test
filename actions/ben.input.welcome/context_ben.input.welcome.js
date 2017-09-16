9645ed59d28ce610c38940489f58895e39fa79f8
Dima Brodsky <Dima Brodsky>
2017-09-16 08:30:28 +0400

    automatic push

--- actions/ben.input.welcome/context_ben.input.welcome.js
+++ actions/ben.input.welcome/context_ben.input.welcome.js
@@ -8,7 +8,7 @@ const apiai = require("apiai");
 module.exports = {
   processor:function(parameterscontextout,requestBody)
   {
-    // default values for original cost of live
+    // after go to next intent
     parameterscontextout["fire_event"]="original_city_question";
     
 
@@ -16,4 +16,39 @@ module.exports = {
   } 
 }
 
+// set variable values for user session in api ai
+var SetNeighborhoodEntityValues=function(sessionId,neighborhoods){
+
+    var app = apiai("b266cf849ba2485a96dcdcee069f60d2");
+
+    var sessionId = sessionId;
+
+    var user_entities = [{
+        name: 'Neighborhood',
+        extend: false,
+        entries: [
+        ]
+    }];
+
+    neighborhoods.forEach(function(neighborhood){
+        user_entities[0].entries.push({value:neighborhood.name,synonyms:[neighborhood.name]});
+    })
+    var user_entities_body = {
+        sessionId: sessionId,
+        entities: user_entities
+    };
+
+    var user_entities_request = app.userEntitiesRequest(user_entities_body);
+
+    user_entities_request.on('response', function(response) {
+        console.log('User entities response: ');
+        console.log(JSON.stringify(response, null, 4));
+    });
+
+    user_entities_request.on('error', function(error) {
+        console.log(error);
+    });
+
+    user_entities_request.end();
+}
 
