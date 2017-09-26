17ed2632b2541843d1003350a8d8126eb1d32a44
Dima Brodsky <Dima Brodsky>
2017-09-26 20:18:27 +0400

    automatic push

--- infographics.js
+++ infographics.js
@@ -4,7 +4,7 @@ const jsonQuery = require('json-query')
 module.exports = {
   infographic_1(parameterscontextout,requestBody){
     
-      parameterscontextout["Infographics"]={number:1,data:{price:1,name:"vasia"}};
+      parameterscontextout["Infographics"]={number:1,data:{price:1,name:"vasia1"}};
   
   }
 };
