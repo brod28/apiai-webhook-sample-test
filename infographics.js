bd048548d34a34a2befd6656579c31673793bf6f
Dima Brodsky <Dima Brodsky>
2017-09-26 20:17:12 +0400

    automatic push

--- infographics.js
+++ infographics.js
@@ -2,23 +2,9 @@
 const jsonQuery = require('json-query')
 
 module.exports = {
-  commutinggroceries_amount_text(parameterscontextout,requestBody){
+  infographic_1(parameterscontextout,requestBody){
     
-    let not_required_transportation=jsonQuery('result.contexts.[*].name', {
-        data: requestBody
-    }).value.indexOf("not_required_transportation");
-
-    let not_required_grocery=jsonQuery('result.contexts.[*].name', {
-        data: requestBody
-    }).value.indexOf("not_required_groceries");
-    
-    
-    if(not_required_grocery!=-1 && not_required_transportation!=-1){
-      parameterscontextout["commutinggroceries_amount_text"]="As you can see that takes your total minimum expense to {AMOUNT}."
-    }
-    else{
-      parameterscontextout["commutinggroceries_amount_text"]="I've added {AMOUNT} to your expenses"
-    }
+      parameterscontextout["Infographics"]={number:1,data:{price:1,name:"vasia"}};
   
   }
 };
